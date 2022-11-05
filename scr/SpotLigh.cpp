a54aeb8232f2d730351a56a396f8d2cdad366a72
Trong Nghia Nguyen <ntnghia.andrei@gmail.com>
2022-11-05 21:31:31 +0700

    pussing code

--- scr/SpotLigh.cpp
+++ scr/SpotLigh.cpp
@@ -1,11 +1,22 @@
 #include "SpotLight.h"
 
-SpotLight::SpotLight()
+SpotLight::SpotLight() : PointLight()
 {
-    pusshing code lol
+    direction = glm::vec3(0.0f, -1.0f, 0.0f);
+    edge = 0.0f;
 }
 
-SpotLigh::SpotLight()
+SpotLight::SpotLight(GLfloat red, GLfloat green, GLfloat blue,
+                    GLfloat aIntensity, GLfloat dIntensity,
+                    GLfloat xPos, GLfloat yPos, GLfloat zPos,
+                    GLfloat xDir, GLfloat yDir, GLfloat zDir,
+                    GLfloat constant, GLfloat linear, GLfloat exponent,
+                    GLfloat edg) : PointLight(red, green, blue, aIntensity, dIntensity,
+                    xPos, yPos, zPos, constant, linear, exponent)
+{
+    direction = glm::vec3(xDir, yDir, zDir);
+    edge = edg;
+}
 SpotLight::~SpotLight()
 {
 }
